735d5af49f70c689be5134c91f6cf4d482319a65
zuramukbaniani <zurabi.mukbaniani.1@btu.edu.ge>
2021-06-15 18:17:17 +0400

    commit 2

--- veryHard.py
+++ veryHard.py
@@ -21,3 +21,48 @@ def convert_to_roman(num):
 				num -= 1000
 				break
 	return result
+
+
+def atbash(txt):
+	ord_a = ord('a')
+	ord_z = ord('z')
+	ord_A = ord('A')
+	ord_Z = ord('Z')
+	result = ''
+	for word in txt:
+		if ord(word) < 65:
+			result += word
+		elif word.islower():
+			to_chr = ord_a + ord_z - ord(word)
+			result += chr(to_chr)
+		elif word.isupper():
+			to_chr = ord_A + ord_Z - ord(word)
+			result += chr(to_chr)
+		elif word.isspace():
+			result += ' '
+	return result
+
+#https://edabit.com/challenge/Fpymv2HieqEd7ptAq
+def split(txt):
+	word = ''
+	num_9 = 0
+	num_0 = 0
+	res_list = []
+	for i in txt:
+		if i == "(":
+			num_9 += 1
+			word += "("
+		elif i == ")":
+			num_0 += 1
+			word += ")"
+			if num_0 == num_9:
+				num_0 = 0
+				num_9 = 0
+				res_list.append(word)
+				word = ''
+	return res_list
+
+#https://edabit.com/challenge/9YfCbQpRPqRLzPCcg
+def will_hit(equation, position):
+	result = eval(equation.replace('y ', str(position[1]) + ' =').replace('x', '*' + str(position[0])))
+	return result
